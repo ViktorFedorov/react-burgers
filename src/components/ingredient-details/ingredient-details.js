34a3dcbce35488177edec16262b5b6ffa44df42e
ViktorF <logforpractik@yandex.ru>
2022-08-18 16:00:34 +0300

    ferac

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -2,28 +2,29 @@ import React from 'react'
 import styles from './ingredient-details.module.css'
 
 const IngredientDetails = ({data, id}) => {
-  const ingredient = data.filter(ing => ing._id === id)
+  const ingredientArr = data.filter(ing => ing._id === id)
+  const [ingredientObj] = ingredientArr
 
-  return (ingredient[0] && (
+  return (ingredientObj && (
     <div className={styles.ingredients}>
-      <img src={ingredient[0].image_large} alt={ingredient[0].name} />
-      <h3 className='mt-4 text text_type_main-medium'>{ingredient[0].name}</h3>
+      <img src={ingredientObj.image_large} alt={ingredientObj.name} />
+      <h3 className='mt-4 text text_type_main-medium'>{ingredientObj.name}</h3>
       <ul className={`mt-8 mb-15 text text_type_main-default text_color_inactive ${styles.nutrients}`}>
         <li className={styles.nutrient}>
           <p>Калории, ккал</p>
-          <p className="text text_type_digits-default">{ingredient[0].calories}</p>
+          <p className="text text_type_digits-default">{ingredientObj.calories}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Белки, г</p>
-          <p className="text text_type_digits-default">{ingredient[0].proteins}</p>
+          <p className="text text_type_digits-default">{ingredientObj.proteins}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Жиры, г</p>
-          <p className="text text_type_digits-default">{ingredient[0].fat}</p>
+          <p className="text text_type_digits-default">{ingredientObj.fat}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Углеводы, г</p>
-          <p className="text text_type_digits-default">{ingredient[0].carbohydrates}</p>
+          <p className="text text_type_digits-default">{ingredientObj.carbohydrates}</p>
         </li>
       </ul>
     </div>)
