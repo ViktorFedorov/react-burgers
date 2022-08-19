b6bc2e91c5d8572c32ecd5de63e75763d7c776e6
ViktorF <logforpractik@yandex.ru>
2022-08-19 18:43:32 +0300

    Перед отправкой на 1е ревью

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -9,24 +9,24 @@ const IngredientDetails = ({data, id}) => {
 
   return (ingredientObj && (
     <div className={styles.ingredients}>
-      <img src={ingredientObj.image_large} alt={ingredientObj.name} />
+      <img className='mt-3' src={ingredientObj.image_large} alt={ingredientObj.name} />
       <h3 className='mt-4 text text_type_main-medium'>{ingredientObj.name}</h3>
       <ul className={`mt-8 mb-15 text text_type_main-default text_color_inactive ${styles.nutrients}`}>
         <li className={styles.nutrient}>
           <p>Калории, ккал</p>
-          <p className="text text_type_digits-default">{ingredientObj.calories}</p>
+          <p className="mt-2 text text_type_digits-default">{ingredientObj.calories}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Белки, г</p>
-          <p className="text text_type_digits-default">{ingredientObj.proteins}</p>
+          <p className="mt-2 text text_type_digits-default">{ingredientObj.proteins}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Жиры, г</p>
-          <p className="text text_type_digits-default">{ingredientObj.fat}</p>
+          <p className="mt-2 text text_type_digits-default">{ingredientObj.fat}</p>
         </li>
         <li className={styles.nutrient}>
           <p>Углеводы, г</p>
-          <p className="text text_type_digits-default">{ingredientObj.carbohydrates}</p>
+          <p className="mt-2 text text_type_digits-default">{ingredientObj.carbohydrates}</p>
         </li>
       </ul>
     </div>)
--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -5,7 +5,7 @@ import styles from './order-details.module.css'
 const OrderDetails = ({orderNumber}) => {
   return (
     <div className={styles.order}>
-      <h2 className={`mt-30 text text_type_digits-large ${styles.numbers}`}>{orderNumber}</h2>
+      <h2 className={`mt-20 text text_type_digits-large ${styles.numbers}`}>{orderNumber}</h2>
       <p className='mt-8 text text_type_main-medium'>Идентификатор заказа</p>
       <div className={`mt-15 ${styles.done}`}></div>
       <p className='mt-15 text text_type_main-default'>Ваш заказ начали готовить</p>
