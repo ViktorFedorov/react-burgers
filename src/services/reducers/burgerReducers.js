282cec9fd9e426f1ba05f4c96fe785f8377a2c32
ViktorF <logforpractik@yandex.ru>
2022-09-06 15:58:56 +0300

    создано начальное состояние хранилища

---
+++ src/services/reducers/burgerReducers.js
@@ -0,0 +1,13 @@
+const initialState = {
+  ingredients: [],
+  ingredientsInConstructor: [],
+  currentlyViewedIngredient: {},
+  order: {}
+}
+
+export const ingredientsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux'
+import {ingredientsReducer} from './burgerReducers'
 
 export const rootReducer = combineReducers({
-
+  ingredientsReducer
 })
