74c5563d6a52fee9ba031df98b37626d87380466
ViktorF <logforpractik@yandex.ru>
2022-11-01 17:20:59 +0300

    refac: выделена главная страница

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,62 +1,26 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import AppHeader from '../app-header/app-header'
-import BurgerIngredients from '../burger-ingredients/burger-ingredients'
-import BurgerConstructor from '../burger-constructor/burger-constructor'
-import Modal from '../modal/modal'
-import IngredientDetails from '../ingredient-details/ingredient-details'
-import {useDispatch, useSelector} from 'react-redux'
-import {DndProvider} from 'react-dnd'
 import {HTML5Backend} from 'react-dnd-html5-backend'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {
-  closeIngredientDetails,
-  getIngredientsThunk,
-  setIngredientDetails
-} from '../../services/actions/ingredients'
 import RegisterPage from '../../pages/register/register'
 import LoginPage from '../../pages/login/login'
 import ForgotPassword from '../../pages/forgot-password/forgot-password'
+import MainPage from '../../pages/main/main'
+import {DndProvider} from 'react-dnd'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const ingredient = useSelector(store => store.detail.ingredient)
-  const open = useSelector(store => store.detail.open)
-
-  useEffect(() => (dispatch(getIngredientsThunk())),[])
-
-  const handlerIngredientClick = (ingredient) => dispatch(setIngredientDetails(ingredient))
-
-  const handlerCloseDetails = () => dispatch(closeIngredientDetails())
-
-  const content = (
-    <main className='content columns'>
-      <BurgerIngredients
-        onClick={handlerIngredientClick} />
-      <BurgerConstructor />
-      <Modal
-        isOpen={open}
-        close={handlerCloseDetails}
-        header='Детали ингредиента'>
-        <IngredientDetails
-          ingredient={ingredient} />
-      </Modal>
-    </main>
-  )
-
   return (
-    <>
     <Router>
       <AppHeader />
       <DndProvider backend={HTML5Backend}>
         <Routes>
-          <Route path='/' element={content}/>
+          <Route path='/' element={<MainPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
       </DndProvider>
-      </Router>
-    </>
+    </Router>
   )
 }
 
---
+++ src/pages/main/main.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from 'react'
+import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients'
+import BurgerConstructor from '../../components/burger-constructor/burger-constructor'
+import Modal from '../../components/modal/modal'
+import IngredientDetails from '../../components/ingredient-details/ingredient-details'
+import {closeIngredientDetails, getIngredientsThunk, setIngredientDetails} from '../../services/actions/ingredients'
+import {useDispatch, useSelector} from 'react-redux'
+
+const MainPage = () => {
+  const dispatch = useDispatch()
+  const ingredient = useSelector(store => store.detail.ingredient)
+  const open = useSelector(store => store.detail.open)
+
+  useEffect(() => (dispatch(getIngredientsThunk())),[])
+
+  const handlerIngredientClick = (ingredient) => dispatch(setIngredientDetails(ingredient))
+  const handlerCloseDetails = () => dispatch(closeIngredientDetails())
+
+  return (
+    <main className='content columns'>
+      <BurgerIngredients
+        onClick={handlerIngredientClick} />
+      <BurgerConstructor />
+      <Modal
+        isOpen={open}
+        close={handlerCloseDetails}
+        header='Детали ингредиента'>
+        <IngredientDetails
+          ingredient={ingredient} />
+      </Modal>
+    </main>
+  )
+}
+
+export default MainPage
